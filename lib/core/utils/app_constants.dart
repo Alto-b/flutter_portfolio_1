1ec2f7e15bcffc63b6e0b6c4cc80e8652663f91b
Alto b <altob282@gmail.com>
2024-04-05 02:41:51 +0530

    first

---
+++ lib/core/utils/app_constants.dart
@@ -0,0 +1,56 @@
+import '../../data/models/custom_service.dart';
+import '../../data/models/project.dart';
+import 'app_assets.dart';
+
+abstract class AppConstants {
+  static const double appBarHeight = 80;
+  static const List<CustomService> services = [
+    CustomService(
+      service: 'MOBILE DEVELOPMENT',
+      logo: AppAssets.androidLogo,
+      description:
+          'I am a Junior mobile developer. I have experience using Dart and Flutter Framework.',
+    ),
+    CustomService(
+      service: 'UI & UX DESIGNING',
+      logo: AppAssets.uiDesignLogo,
+      description:
+          'I design beautiful web interfaces with Figma and Adobe XD. I design beautiful web interfaces with Figma and Adobe XD.',
+    ),
+    CustomService(
+      service: 'WEB DEVELOPMENT',
+      logo: AppAssets.scrappingLogo,
+      description:
+          'I can collect content and data from the internet then manipulate and analyze as needed.',
+    ),
+  ];
+  static const List<Project> projects = [
+    Project(
+      name: 'Live score',
+      imageUrl:
+          'https://drive.google.com/uc?id=1ITN6reINiIaM-6j3QqxrwtvADZgLNvwB',
+      description:
+          'A football application to display all related matches and rankings in all international and local tournaments',
+      githubRepoLink: 'https://github.com/radyhaggag/live_score',
+      previewLink: 'https://youtu.be/HSIosFd6Sys',
+    ),
+    Project(
+      name: 'Live score',
+      imageUrl:
+          'https://drive.google.com/uc?id=1ITN6reINiIaM-6j3QqxrwtvADZgLNvwB',
+      description:
+          'A football application to display all related matches and rankings in all international and local tournaments',
+      githubRepoLink: 'https://github.com/radyhaggag/live_score',
+      previewLink: 'https://youtu.be/HSIosFd6Sys',
+    ),
+    Project(
+      name: 'Live score',
+      imageUrl:
+          'https://drive.google.com/uc?id=1ITN6reINiIaM-6j3QqxrwtvADZgLNvwB',
+      description:
+          'A football application to display all related matches and rankings in all international and local tournaments',
+      githubRepoLink: 'https://github.com/radyhaggag/live_score',
+      previewLink: 'https://youtu.be/HSIosFd6Sys',
+    ),
+  ];
+}
---
+++ lib/core/utils/social_links.dart
@@ -0,0 +1,6 @@
+abstract class SocialLinks {
+  static const String facebook = "https://www.facebook.com/radyhaggag5";
+  static const String github = "https://github.com/Alto-b";
+  static const String instagram = "https://www.instagram.com/radyhaggag5";
+  static const String linkedin = "https://www.linkedin.com/in/alto-b-puthethu/";
+}
